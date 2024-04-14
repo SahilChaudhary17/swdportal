a8b2224c7ad067f2e0f8c2845fc875ce41217eac
Sahil Chaudhary <sahilch38@gmail.com>
2024-04-14 11:52:10 +0530

    update icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,24 +2,47 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
-const inter = Inter({subsets:['latin'],  weight: '400',});
+const inter = Inter({ subsets: ['latin'], weight: '400' });
 
 export const metadata = {
   title: "SW Discipline Portal",
   description: "Office of Students' Welfare",
+  icons: [
+    {
+      rel:'icon',
+      type:'image/x-icon',
+      href:'/favicon.ico'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon/favicon-32x32.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon/favicon-16x16.png'
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/favicon/apple-touch-icon.png'
+    },
+    {
+      rel: 'manifest',
+      href: '/favicon/site.webmanifest'
+    }
+  ]
 };
 
-// {/* <link href="https://fonts.googleapis.com/css2?family=Train+One&display=swap" rel="stylesheet"></link> */}
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class"
-          defaultTheme="system"
-          enableSystem
-          // disableTransitionOnChange
-          >
-           {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
         </ThemeProvider>
       </body>
     </html>
